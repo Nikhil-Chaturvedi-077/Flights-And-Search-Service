760ff1d7a6f49d3eaf569be06a8e8d757caf0236
Nikhil Chaturvedi <nikhilchaturvedi07.com>
2025-04-10 14:19:59 +0530

    Setup basic dotenv

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require('dotenv').config()
+
+const setupAndStartServer = async()=>{
+
+    //create the express object
+    const app = express();
+    const PORT = 3000;
+    app.listen(PORT,()=>{
+        console.log(`server started at ${PORT}`);
+        console.log(process.env) 
+    });
+}
+
+setupAndStartServer();
